a09d3ac5b6006a401aa4cce7d2d9c7b1f4f46f1a
hyugoshirai <148725261+hyugoshirai@users.noreply.github.com>
2024-06-11 12:09:12 -0400

    Histogram for individuals count click on marker for info

--- app.R
+++ app.R
@@ -34,6 +34,7 @@ ui <- bootstrapPage(
                   choices = species_choices,
                   multiple = TRUE),
                 gt::gt_output("species_in_area"),
+                plotOutput("histogram"),
                 htmlOutput("species_list_text")),
   absolutePanel(bottom = 10, left = 10,
                 style = "background-color: rgba(255,255,255,0.7);padding: 10px 30px 10px 30px;border-radius: 20px;",
@@ -45,26 +46,49 @@ ui <- bootstrapPage(
                   value = c(as.Date("2021-01-01"), as.Date("2024-12-31")),
                   timeFormat = "%Y-%m-%d"))
 )
+
+
 server <- function(input, output, session) {
   
+  # Reactive expression to filter data based on map bounds
   df_bounds <- reactive({
-    if (is.null(input$map_bounds))
-      return(df_sites[FALSE,])
-    bounds <- input$map_bounds
-    latRng <- range(bounds$north, bounds$south)
-    lngRng <- range(bounds$east, bounds$west)
+    if (is.null(input$map_bounds))  # Check if map bounds are null
+      return(df_sites[FALSE,])  # Return empty data frame if bounds are null
+    bounds <- input$map_bounds  # Get map bounds
+    latRng <- range(bounds$north, bounds$south)  # Get latitude range
+    lngRng <- range(bounds$east, bounds$west)  # Get longitude range
     
+    # Filter data based on latitude and longitude ranges
     subset(df_sites,
            decimalLatitude >= latRng[1] & decimalLatitude <= latRng[2] &
              decimalLongitude >= lngRng[1] & decimalLongitude <= lngRng[2])
   })
   
-  output$species_in_area <- gt::render_gt({
-    df <- df_bounds()
+  # Reactive expression to filter data based on date range and selected species
+  df_date_spp <- reactive({
+    # Combine year, month, and day into a single date column
+    species_choices <- df_sites %>% tidyr::unite("sight_date",
+                                                 year, month, day,
+                                                 sep = "-") %>%
+      dplyr::mutate(sight_date = as.Date(sight_date)) %>%
+      dplyr::filter(sight_date > input$day_month[1],  # Filter by start date
+                    sight_date < input$day_month[2])  # Filter by end date
+    
+    # Filter further based on selected species names
     if (!is.null(input$sci_name) && length(input$sci_name) > 0) {
-      df <- df %>%
-        filter(str_detect(species_list, paste(input$sci_name, collapse = "|")))
+      return(species_choices %>%
+               filter(str_detect(species_list, paste(input$sci_name, collapse = "|"))))
+    } else {
+      return(species_choices)  # Return species choices if no species selected
     }
+  })
+  
+  # Render the table of most recurring species in the area
+  output$species_in_area <- gt::render_gt({
+    df <- df_bounds()  # Initially get the data within bounds
+    df <- df_date_spp() # Get the data within time range
+
+    # Perform data transformations and create visualization
     df %>%
       select(species_list) %>%
       separate_rows(species_list, sep = ",") %>%
@@ -73,72 +97,97 @@ server <- function(input, output, session) {
       rename("Species" = "species_list") %>%
       gt::gt() %>%
       gt::tab_options(table.font.size = "12pt", heading.title.font.size = "14pt") %>%
-      gt::tab_header(title = "Most observed species in area") %>%
+      gt::tab_header(title = "Most recurring species in area") %>%
       gtExtras::gt_plt_bar(column = Count, color = "darkgreen", scale_type = "number")
   })
   
+  
+  # Render the histogram of individuals counts over time
+  output$histogram <- renderPlot({
+    # Initially get the data within bounds
+    df <- df_bounds()
+    
+    # Filter the data further based on date and selected species
+    df <- df_date_spp()
+    
+    # Create histogram of individuals counts over time
+    ggplot(df, aes(x = sight_date, y = species_count)) +
+      geom_histogram(stat = "identity", fill = "blue") +
+      labs(x = "Date", y = "Sum of Species Counts", title = "Species Counts Over Time") +
+      theme_minimal()
+  })
+  
+
+  # Render the leaflet map
   output$map <- renderLeaflet({
     leaflet(options = leafletOptions(preferCanvas = TRUE)) %>%
       addTiles() %>%
       fitBounds(lng1 = -75, lat1 = -15, lng2 = -45, lat2 = 5)
   })
   
+  # Reactive expression to update species choices based on selected species
   sci_name_choices <- reactive({
     names(species_choices) <- species_choices
     
+    # Return species choices if no species selected
     if (is.null(input$sci_name) || length(input$sci_name) == 0 || input$sci_name == "") {
       return(species_choices)
     }
     species_choices[str_detect(species_choices, paste(input$sci_name, collapse = "|"))]
   })
   
+  # Observe changes in selected species and update the picker input accordingly
   observe({
     updatePickerInput(session, "sci_name", choices = species_choices, selected = input$sci_name)
   })
   
-  # output$species_list_text <- renderUI({
-  #   if (!is.null(input$map_marker_click)) {
-  #     marker_data <- str_split(input$map_marker_click, "_", simplify = TRUE)[1,]
-  #     marker_month <- lubridate::month(as.Date(glue::glue("2021-{marker_data[4]}-01")), label = TRUE, abbr = FALSE)
-  #     species_list <- df_sites %>%
-  #       filter(site_id == input$map_marker_click$id) %>%
-  #       select(species_list) %>%
-  #       unlist() %>%
-  #       str_trim() %>%
-  #       str_split(",") %>%
-  #       unlist() %>%
-  #       head(10) %>%
-  #       paste(collapse = "</span><span class='species_item'>")
-  #     header <- glue::glue("<span class='species_list_header'> Most popular bird species found at <br> {marker_data[1]},{marker_data[1]} in the month of {marker_month}</span>")
-  #     species_list <- paste(header, "<span class='species_item'>", species_list, "</span>", collapse = '')
-  #     return(HTML(species_list))
-  #   } else {
-  #     HTML("<span> Click on a marker to see the list of birds ordered by overall popularity </span>")
-  #   }
-  # })
-  
-  df_react <- reactive({
-    species_choices <- df_sites %>% tidyr::unite("sight_date",
-                                                 year, month, day,
-                                                 sep = "-") %>%
-      dplyr::mutate(sight_date = as.Date(sight_date)) %>%
-      dplyr::filter(sight_date > input$day_month[1],
-                    sight_date < input$day_month[2])
-    
-    if (!is.null(input$sci_name) && length(input$sci_name) > 0) {
-      print(input$sci_name)
-      return(species_choices %>%
-               filter(str_detect(species_list, paste(input$sci_name, collapse = "|"))))
+  # Render the UI for displaying marker information
+  output$species_list_text <- renderUI({
+    if (!is.null(input$map_marker_click)) {
+      marker_id <- input$map_marker_click$id
+      
+      # Extract data for the clicked marker
+      site_data <- df_sites %>% filter(site_id == marker_id)
+      
+      # Extract information
+      species_list <- site_data$species_list
+      decimal_longitude <- round(site_data$decimalLongitude,5)  
+      decimal_latitude <- round(site_data$decimalLatitude,5)
+      date <- paste(site_data$year,"/",site_data$month,"/",site_data$day)
+      count <- site_data$species_count
+      
+      # Create header and information
+      header <- glue::glue("<span class='species_list_header' 
+      style='color: green; 
+      font-weight: bold; 
+      font-size: 14px;'>
+      Marker info:<br>")
+      
+      info <- glue::glue("<span class='species_list_info' 
+      style='color: black; 
+      font-weight: lighter; 
+      font-size: 12px;'>
+        Species: {species_list}<br>
+        Longitude: {decimal_longitude}<br>
+        Latitude: {decimal_latitude}<br>
+        Date: {date}<br>
+        Count: {count} individual (s)")
+      
+      # Combine header and information and return as HTML
+      FullText <- paste(header, info)
+      return(HTML(FullText))
     } else {
-      return(species_choices)
+      return(HTML("<span>Click on a marker to see the species data</span>"))  # Default message
     }
   })
   
+  # Create color palette for legend
   count_palet <- colorBin(palette = "Dark2", bins = 3, pretty = TRUE,
                           domain = range(df_sites$species_count))
   
+  # Observe changes in filtered data and update leaflet map accordingly
   observe({
-    leafletProxy("map", data = df_react()) %>%
+    leafletProxy("map", data = df_date_spp()) %>%
       clearMarkerClusters() %>%
       clearShapes() %>%
       clearMarkers() %>%
@@ -156,6 +205,8 @@ server <- function(input, output, session) {
       )
   })
 }
+
+
 
 # Run the application
 shinyApp(ui = ui, server = server)
